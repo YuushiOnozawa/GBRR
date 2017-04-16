a9c9a6f163b73996ec0e319d09e0949b73ad2d97
Yuushi Onozawa <yonozawa0815@gmail.com>
2017-04-16 19:30:28 +0900

    Stream取得できた

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,15 @@
 import fs from 'fs';
 import http from 'http';
 import socket from 'socket.io';
+import ApiKeys from '../../twitterKeys.json';
+const twitter = require('twitter');
+
+const client = new twitter({
+    consumer_key: ApiKeys.consumer_key,
+    consumer_secret: ApiKeys.consumer_secret,
+    access_token_key: ApiKeys.access_token_key,
+    access_token_secret: ApiKeys.access_token_secret
+});
 
 const server = http.createServer();
 const io = socket.listen(server);
@@ -12,6 +21,19 @@ server.on('request', (req, res) => {
 });
 
 server.listen(8080);
+
+client.stream('statuses/filter',
+    {track: 'グラブル'},
+    (stream) => {
+        stream.on('data', (text) => {
+            console.log(text);
+        });
+
+        stream.on('error', (err) => {
+            console.log(err);
+        });
+    }
+);
 
 io.sockets.on('connection', (socket) => {
     socket.emit(
@@ -25,8 +47,5 @@ io.sockets.on('connection', (socket) => {
     setInterval(() => {
         console.log(new Date());
         io.sockets.emit('update', {value: new Date()});
-    }, 5000);
+    }, 10000);
 });
-
-
-
