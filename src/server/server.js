b45dc60f4ed8ff5bd700e5a76316b37687b586d4
Yuushi Onozawa <yonozawa0815@gmail.com>
2017-04-16 16:19:21 +0900

    通信できた

--- src/server/server.js
+++ src/server/server.js
@@ -3,12 +3,30 @@ import http from 'http';
 import socket from 'socket.io';
 
 const server = http.createServer();
+const io = socket.listen(server);
 
 server.on('request', (req, res) => {
-    const stream = fs.createReadStream('index.html');
+    const stream = fs.createReadStream('index.html', 'utf-8');
     res.writeHead(200, {'Content-Type': 'text/html'});
     stream.pipe(res);
 });
 
-socket.listen(server);
 server.listen(8080);
+
+io.sockets.on('connection', (socket) => {
+    socket.emit(
+        'greeting',
+        {message: 'hello'},
+        (data) => {
+            console.log(data);
+        }
+    );
+
+    setInterval(() => {
+        console.log(new Date());
+        io.sockets.emit('update', {value: new Date()});
+    }, 5000);
+});
+
+
+
